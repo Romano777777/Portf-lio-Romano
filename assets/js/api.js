8a5d78dbc11426a06b7aafbc48a89354484bdd1f
Romano <br0567575@gmail.com>
2024-05-17 11:36:44 -0300

    inicial commit

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,8 @@
+
+
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Romano777777/js-developer-portfolio/main/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/Portf-lio-Romano/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
